20f36de842f21bea39715ec0c990048138343937
Solomon Danso <solomondanso2023@gmail.com>
2025-05-05 06:36:42 +0000

    Learning Plus

--- Fast Api/Main.py
+++ Fast Api/Main.py
@@ -1,6 +1,7 @@
 from Interfaces import Item
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 from pydantic import BaseModel
+from typing import Annotated
 
 
 
@@ -75,3 +76,33 @@ async def create_item(item: Item):
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
+
+@app.put("/ItemsWithParams/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+    """api url: http://127.0.0.1:8000/ItemsWithParams/2?q=woow
+    
+    {
+  "item_id": 2,
+  "name": "solo",
+  "description": "testing",
+  "price": 45,
+  "tax": 30,
+  "q": "woow"
+}
+    
+    """
+
+@app.get("/anotatedItems")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+
